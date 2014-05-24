6e3ad9471c3ab234c3de1319d32813fa3640e08a
benny <zhangbx@outlook.com>
2014-05-24 00:46:23 -0400

    Initial Commit

---
+++ Roommates/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Roommates
+//
+//  Created by Zhang Boxuan on 14-5-24.
+//  Copyright (c) 2014年 Boxuan Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RoommatesTests/RoommatesTests.m
@@ -0,0 +1,34 @@
+//
+//  RoommatesTests.m
+//  RoommatesTests
+//
+//  Created by Zhang Boxuan on 14-5-24.
+//  Copyright (c) 2014年 Boxuan Zhang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RoommatesTests : XCTestCase
+
+@end
+
+@implementation RoommatesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
